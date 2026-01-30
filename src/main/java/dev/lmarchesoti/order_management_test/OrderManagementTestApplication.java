193973e0944886f71d12d21942d90a5a5ecddd26
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2026-01-30 20:03:25 -0300

    Initial commit

---
+++ src/main/java/dev/lmarchesoti/order_management_test/OrderManagementTestApplication.java
@@ -0,0 +1,13 @@
+package dev.lmarchesoti.order_management_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lmarchesoti/order_management_test/OrderManagementTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lmarchesoti.order_management_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
